196bd546473236fa68a217ad28da74e066c7ea10
GermanEduardoVega <103130114+GermanEduardoVega@users.noreply.github.com>
2022-06-15 12:04:08 -0300

    Modulo Pago y confirmacion

--- PROYECTO/interfaces/iConfirmacion.py
+++ PROYECTO/interfaces/iConfirmacion.py
@@ -50,8 +50,7 @@ class Pago:
         tk.Label(frame, text="Precio Total").place(
             relx=0.01, rely=0.50)
         #Creacion de los Botones
-        tk.Button(frame, text="PAGO",
-                  command=self.verPago).place(relx=0.05, rely=0.60)
+
         tk.Button(frame, text="SIGUIENTE",
                   command=self.siguienteInterfaz).place(relx=0.80, rely=0.80)
 
@@ -98,55 +97,14 @@ class Pago:
         #tk.Button(frame, text="Atras",
         #           command=self.atras).place(relx=0.01, rely=0.9)
 
-    def verPago(self):
-        self.verPago = Toplevel()
-        self.verPago.title("PAGO")
-        screenWidth = self.verPago.winfo_screenwidth()
-        screenHeight = self.verPago.winfo_screenheight()
-        # Establece ancho de la ventana.
-        width = 400
-        # Establece altura de la ventana.
-        height = 300
-        left = (screenWidth - width) / 2
-        top = (screenHeight - height) / 2
-        self.verPago.geometry("%dx%d+%d+%d" % (width, height, left, top))
-        self.verPago.resizable(0, 0)
 
-        #Creacion del Frame
-        frameVerPago = LabelFrame(self.verPago)
-        frameVerPago.place(relwidth=1, relheight=1)
 
-        #Creacion de los Label
-        tk.Label(frameVerPago, text="Ingrese Codigo de Verificación:").place(
-            relx=0.30, rely=0.3)
 
-        #Crecion de los Textbox
-        self.codigoVer = tk.Entry(frameVerPago)
-        self.codigoVer.focus()
-        self.codigoVer.place(relx=0.50, rely=0.3)
-
-        #Creacion del Botones
-        tk.Button(frameVerPago, text="Validar",
-                  command=self.validarPago).place(relx=0.50, rely=0.5)
-        tk.Button(frameVerPago, text="Atras",
-                  command=self.verPago.withdraw).place(relx=0.01, rely=0.01)
-
-    def validarPago(self):
-        if (self.codigoVer.get()):
-            if (self.codigoVer.get() == "123"):  # Valida el codigo ingresado con el codigo tecnico
-                messagebox._show("Pago", "Codigo Aceptado")
-                self.verPago.withdraw()
-            else:
-                messagebox.showwarning("Pago", "Codigo Erroneo")
-                self.codigoVer.focus()
-        else:
-            messagebox.showwarning(
-                "Error", "Los campos no pueden estar vacíos")
-            self.codigoVer.focus()
 
     def siguienteInterfaz(self):
         self.wind.withdraw()
         #Conexion con siguiente interfaz
+
 
 
 app = Pago(Tk())
