1c018e375054a1941fc8a5442c649b08260b0f67
Araya-Alejo <alejo.araya@alumnos.frm.utn.edu.ar>
2022-06-16 15:37:12 -0300

    Correcion mail

--- Proyecto/servicios/mail.py
+++ Proyecto/servicios/mail.py
@@ -1,14 +1,18 @@
 import smtplib
 import SO
+import interfaces.ESTANDARES
 
-conexion = smtplib.SMTP(host='smtp.gmail.com', port=587)
-conexion.ehlo()
+def ENVIAR_CORREO(mensaje, MAIL_OTHER_USER):
+    try:
+        conexion = smtplib.SMTP(host='smtp.gmail.com', port=587)
+        conexion.ehlo()
 
-conexion.starttls()
+        conexion.starttls()
 
-conexion.login(user= SO.MAIL_USER, password = SO.MAIL_PASSWORD)
-
-mensaje = "Subject: AlquilaYa\nEste es la factura del auto, muchas gracias por trabajar con nosotros."
-conexion.sendmail(from_addr= SO.MAIL_USER, to_addrs=SO.MAIL_USER,msg= mensaje)
-
-conexion.quit()
+        conexion.login(user= SO.MAIL_USER, password = SO.MAIL_PASSWORD)
+        mensaje = "Subject: AlquilaYa\nEste es la factura del auto, muchas gracias por trabajar con nosotros."
+        conexion.sendmail(from_addr= SO.MAIL_USER, to_addrs=MAIL_OTHER_USER,msg= mensaje)
+    except ValueError:
+        MENSAJE_CONSOLA("Mail no valido", validar)
+    finally:
+        conexion.quit()
